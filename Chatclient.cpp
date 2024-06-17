78c5d6471abb8af68d5c24660dcbbb1cb765155c
Bogdan <159520057+ModestBody@users.noreply.github.com>
2024-06-17 21:09:59 +0300

    Add files via upload

---
+++ Chatclient.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <thread>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+SOCKET clientSocket;
+
+void receiveMessages() {
+    char buffer[1024];
+    int result;
+    while (true) {
+        result = recv(clientSocket, buffer, 1024, 0);
+        if (result > 0) {
+            buffer[result] = '\0';
+            cout << buffer << endl;
+        }
+        else {
+            break;
+        }
+    }
+    closesocket(clientSocket);
+}
+
+int main() {
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        cout << "WSAStartup failed: " << iResult << endl;
+        return 1;
+    }
+
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        cout << "Error creating socket: " << WSAGetLastError() << endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(12345);
+    inet_pton(AF_INET, "192.168.92.1", &serverAddr.sin_addr); 
+
+    if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cout << "Connect failed: " << WSAGetLastError() << endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    string nickname;
+    cout << "Choose username: ";
+    getline(cin, nickname);
+
+    thread(receiveMessages).detach();
+
+    string message;
+    while (true) {
+        getline(cin, message);
+        message = nickname + ": " + message;
+        send(clientSocket, message.c_str(), message.length(), 0);
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+    return 0;
+}
---
+++ Chatserver.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <thread>
+#include <vector>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+vector<SOCKET> clients;
+
+void broadcast(const string& message, SOCKET sender) {
+    for (SOCKET client : clients) {
+        if (client != sender) {
+            send(client, message.c_str(), message.length(), 0);
+        }
+    }
+}
+
+void handleClient(SOCKET client) {
+    char buffer[1024];
+    int result;
+    while (true) {
+        result = recv(client, buffer, 1024, 0);
+        if (result <= 0) {
+            break;
+        }
+        buffer[result] = '\0';
+        broadcast(buffer, client);
+    }
+
+    closesocket(client);
+    clients.erase(remove(clients.begin(), clients.end(), client), clients.end());
+}
+
+int main() {
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        cout << "WSAStartup failed: " << iResult << endl;
+        return 1;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        cout << "Error creating socket: " << WSAGetLastError() << endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(12345);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cout << "Bind failed: " << WSAGetLastError() << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        cout << "Listen failed: " << WSAGetLastError() << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Server is working and waiting for connections" << endl;
+
+    while (true) {
+        SOCKET clientSocket = accept(serverSocket, NULL, NULL);
+        if (clientSocket == INVALID_SOCKET) {
+            cout << "Accept failed: " << WSAGetLastError() << endl;
+            closesocket(serverSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        clients.push_back(clientSocket);
+        thread(handleClient, clientSocket).detach();
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+    return 0;
+}
